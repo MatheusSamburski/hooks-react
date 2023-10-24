72e4cba302019fa6ddd247323a71bb557c642c78
MatheusSamburski <mateus41789@gmail.com>
2023-10-24 18:18:35 -0300

    useEffect and state

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { HookUseEffect } from "./components/HookUseEffect";
+import { HookUseState } from "./components/HookUseState";
+
+export function App() {
+  return (
+    <>
+      <HookUseState />
+      <HookUseEffect />
+    </>
+  );
+}
