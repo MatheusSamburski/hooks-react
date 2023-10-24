85879422d311b1c4c59b9e43333a09c9b21e0dd6
MatheusSamburski <mateus41789@gmail.com>
2023-10-24 18:48:18 -0300

    useCallback e memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { HookUseEffect } from "./components/HookUseEffect";
-import { HookUseState } from "./components/HookUseState";
+import { HookUseEffect } from "./hooks/HookUseEffect";
+import { HookUseMemo } from "./hooks/HookUseMemo";
+import { HookUseState } from "./hooks/HookUseState";
+import { HookUseCallback } from "./hooks/hook-use-callback/HookUseCallback";
 
 export function App() {
   return (
     <>
       <HookUseState />
       <HookUseEffect />
+      <HookUseCallback />
+      <HookUseMemo />
     </>
   );
 }
